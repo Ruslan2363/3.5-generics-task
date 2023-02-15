f10c0af9b207dcbf95e80eb22799d506d2d30d4d
ruslan <r.shalapay2001@gmail.com>
2023-02-15 19:55:51 +0200

    Пробував узальнити щось

--- src/main/java/ua/pp/voronin/serhii/task/CompleteMethods.java
+++ src/main/java/ua/pp/voronin/serhii/task/CompleteMethods.java
@@ -4,38 +4,43 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import static java.util.List.of;
+
 public class CompleteMethods {
 
     public static void main(String[] args) {
-        getUniqueMeasurableItems(List.of(new Apple(), new WaterMelon(), new Tomato())); // 1
-        getUniqueMeasurableItems(List.of(new Coin(), new Toy(), new Chair())); // 2
-        getUniqueMeasurableItems(List.of(new Water(), new Juice(), new Oil())); // 3
-        var packedBeverages = getUniqueMeasurableItems(List.of(new CanOfCoke(), new BottleOfJuice())); // 4
+        getUniqueMeasurableItems (List.of(new Apple(), new WaterMelon(), new Tomato())); // 1
+        getUniqueMeasurableItems(of(new Coin(), new Toy(), new Chair())); // 2
+        getUniqueMeasurableItems(of(new Water(), new Juice(), new Oil())); // 3
+        var packedBeverages  = getUniqueMeasurableItems(of(new CanOfCoke(), new BottleOfJuice())); // 4
     }
 
-    public static <T /* тут вписати визначення типу T */> Set<T> getUniqueMeasurableItems(List<T> list) {
-        // some specific processing to happen here
+    public static <T> Set<T> getUniqueMeasurableItems(List<T> list) {
         return new HashSet<T>(list);
     }
 
-    private interface MeasuredInKilograms {}
+    private interface MeasuredInKilograms<T1> {
 
-    private static class Apple implements MeasuredInKilograms {}
-    private static class WaterMelon implements MeasuredInKilograms {}
-    private static class Tomato implements MeasuredInKilograms {}
+    }
 
-    private interface Countable {}
+    private static class Apple<T1> implements MeasuredInKilograms<T1> {}
+    private static class WaterMelon <T1>implements MeasuredInKilograms <T1>{}
+    private static class Tomato<T1> implements MeasuredInKilograms <T1>{}
 
-    private static class Coin implements Countable {}
-    private static class Toy implements Countable {}
-    private static class Chair implements Countable {}
+    private interface Countable<T2> {}
 
-    private interface MeasuredInLiters {}
+    private static class Coin<T2> implements Countable <T2>{}
+    private static class Toy<T2> implements Countable<T2> {}
+    private static class Chair<T2> implements Countable <T2>{}
 
-    private static class Water implements MeasuredInLiters {}
-    private static class Juice implements MeasuredInLiters {}
-    private static class Oil implements MeasuredInLiters {}
+    private interface MeasuredInLiters<T3> {
 
-    private static class CanOfCoke implements Countable, MeasuredInLiters {}
-    private static class BottleOfJuice implements Countable, MeasuredInLiters {}
+    }
+
+    private static class Water <T3>implements MeasuredInLiters <T3>{}
+    private static class Juice<T3> implements MeasuredInLiters <T3>{}
+    private static class Oil <T3>implements MeasuredInLiters <T3>{}
+
+    private static class CanOfCoke<T4> implements Countable, MeasuredInLiters {}
+    private static class BottleOfJuice<T5> implements Countable, MeasuredInLiters {}
 }
